dda08fa16a6a8666897eaa9c2ef69e55c747dce0
Kelby Faessler <kmfaessler@gmail.com>
2022-01-07 11:10:31 -0800

    add navigation and indent keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -5,5 +5,28 @@ local opts = {
 
 local keymap = vim.api.nvim_set_keymap
 
+-- Normal --
+-- Window Navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Window Resize
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Buffer Navigation
+keymap("n", "<s-l>", ":bnext<CR>", opts)
+keymap("n", "<s-h>", ":bprevious<CR>", opts)
+
 -- NvimTree
 keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
+
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
