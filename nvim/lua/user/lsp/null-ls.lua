f95cd4f1a57a02b28ac6767845f03ecaedb8268a
Kelby Faessler <kelby.faessler@foldapp.com>
2022-11-22 18:15:37 -0500

    null-ls upgrade

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -5,6 +5,9 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
+-- to support formatting on save (https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save)
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup {
   debug = false,
   sources = {
@@ -14,6 +17,32 @@ null_ls.setup {
     formatting.black.with { extra_args = { "--fast" } },
     -- formatting.yapf,
     formatting.stylua,
-    diagnostics.flake8,
+    -- diagnostics.pylint,
   },
+  -- you can reuse a shared lspconfig on_attach callback here
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          vim.lsp.buf.formatting_sync()
+        end,
+      })
+    end
+  end,
 }
+
+-- NOTE: haven't tried this yet
+-- To filter the format calls so only null-ls receives the format request and not other available
+-- formatters e.g. language servers
+-- local callback = function()
+--     vim.lsp.buf.format({
+--         bufnr = bufnr,
+--         filter = function(client)
+--             return client.name == "null-ls"
+--         end
+--     })
+-- end,
