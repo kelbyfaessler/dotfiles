996eadb2e60e3b3b77267cffdab3ef825419ebf2
Kelby Faessler <kmfaessler@gmail.com>
2023-01-16 16:21:42 -0500

    adjust javascript indentations

--- nvim/lua/user/autocommands.lua
+++ nvim/lua/user/autocommands.lua
@@ -73,8 +73,8 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 vim.api.nvim_create_autocmd({ "FileType" }, {
     pattern = { "javascript", "vue", "html", "javascriptreact", "svelte" },
     callback = function()
-        vim.opt_local.shiftwidth = 4
-        vim.opt_local.softtabstop = 4
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.softtabstop = 2
         vim.opt_local.expandtab = true
     end,
 })
