403b48b650828282972dd78d2d19a585f299be98
Kelby Faessler <kmfaessler@gmail.com>
2023-02-18 14:56:20 -0500

    adjust lualine config

--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -33,6 +33,25 @@ local location = {
     padding = 0,
 }
 
+local filename = {
+    "filename",
+    file_status = true, -- Displays file status (readonly status, modified status)
+    newfile_status = false, -- Display new file status (new file means no write after created)
+    path = 3, -- 0: Just the filename
+    -- 1: Relative path
+    -- 2: Absolute path
+    -- 3: Absolute path, with tilde as the home directory
+
+    shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+    -- for other components. (terrible name, any suggestions?)
+    symbols = {
+        modified = "[+]", -- Text to show when the file is modified.
+        readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+        unnamed = "[No Name]", -- Text to show for unnamed buffers.
+        newfile = "[New]", -- Text to show for newly created file before first write
+    },
+}
+
 local spaces = function()
     return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
 end
@@ -49,9 +68,9 @@ lualine.setup({
     },
     sections = {
         lualine_a = { "mode" },
-        lualine_b = { "branch" },
-        lualine_c = { diagnostics },
-        lualine_x = { diff, spaces, "encoding", filetype },
+        lualine_b = {},
+        lualine_c = { filename },
+        lualine_x = { spaces, "encoding" },
         lualine_y = { location },
         lualine_z = { "progress" },
     },
