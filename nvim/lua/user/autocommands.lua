65f2060b0476fedd7f19bb125fd3113963266e4a
Kelby Faessler <kmfaessler@gmail.com>
2024-01-01 14:55:54 -0500

    port for launch.nvim commit 6eb2c3c

--- nvim/lua/user/autocommands.lua
+++ nvim/lua/user/autocommands.lua
@@ -26,12 +26,6 @@ vim.api.nvim_create_autocmd({ "VimResized" }, {
     end,
 })
 
-vim.api.nvim_create_autocmd({ "CmdWinEnter" }, {
-    callback = function()
-        vim.cmd("quit")
-    end,
-})
-
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
     callback = function()
         vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
