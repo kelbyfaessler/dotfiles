a5af46041d9fa1c71b2d5565bbd4f7b245daf9de
Kelby Faessler <kelby.faessler@foldapp.com>
2025-04-03 15:45:06 -0400

    modicator tweaks

--- nvim/lua/user/extras/modicator.lua
+++ nvim/lua/user/extras/modicator.lua
@@ -1,11 +1,12 @@
 local M = {
   "mawkler/modicator.nvim",
+  commit = "45b64561e109cd04de551b081fb947b4f856009e",
   event = "BufEnter",
 }
 
 function M.config()
   require("modicator").setup({
-    show_warnings = true,
+    show_warnings = false,
     highlights = {
       defaults = {
         bold = true,
